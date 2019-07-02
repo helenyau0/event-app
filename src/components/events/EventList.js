5c7f93ddfa16971386703b684ec789c220949395
helen <>
2019-07-02 13:20:32 -0700

    create EventList component and integrate api to get all events

---
+++ src/components/events/EventList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import events from '../../apis/events';
+
+class EventList extends React.Component {
+  state = { allEvents: [] };
+
+  async componentDidMount() {
+    try {
+      const response = await events.get('/events/?limit=100&offset=0');
+      this.setState({ allEvents: response.data.results });
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  render() {
+    const { allEvents } = this.state;
+
+    return (
+      <div className="ui five cards">
+        {allEvents.map(event => {
+          return (
+            <div className="card" key={event.id}>
+              <div className="image">
+                <img src={event.logo_uri} alt={event.name} />
+              </div>
+              <div className="extra">{event.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default EventList;
