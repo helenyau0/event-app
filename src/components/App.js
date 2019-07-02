c76c02337c7874ac3a5c873fb4c872acfb978de6
helen <>
2019-07-02 13:50:00 -0700

    hook routing to App component and create route for EventList

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import EventList from './events/EventList';
 
 const App = () => {
   return (
     <div className="ui container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events" exact component={EventList} />
+        </Switch>
+      </BrowserRouter>
       <EventList />
     </div>
   );
