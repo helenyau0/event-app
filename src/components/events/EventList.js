52e1824390658dc836f0a04d5a715e1e159cd0bd
helen <>
2019-07-06 17:06:45 -0700

    use api to get all events from EventList store

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -2,35 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
 
-import events from '../../apis/events';
-
 class EventList extends React.Component {
-  async componentDidMount() {
-    try {
-      const response = await events.get('/events/?limit=100&offset=0');
-      const { EventListStore } = this.props;
-      EventListStore.allEvents = response.data.results;
-    } catch (error) {
-      console.error(error);
-    }
+  componentDidMount() {
+    this.props.EventListStore.getAllEvents();
   }
 
   render() {
     const { EventListStore } = this.props;
     return (
-      <div className="ui five cards">
-        {EventListStore.allEvents.map(event => {
-          return (
-            <div className="card" key={event.id}>
-              <div className="image">
-                <img src={event.logo_uri} alt={event.name} />
+      <div>
+        <div className="ui five cards">
+          {EventListStore.allEvents.map(event => {
+            return (
+              <div className="card" key={event.id}>
+                <div className="image">
+                  <img src={event.logo_uri} alt={event.name} />
+                </div>
+                <div className="extra">
+                  <Link to={`/events/${event.id}`}>{event.name}</Link>
+                </div>
               </div>
-              <div className="extra">
-                <Link to={`/events/${event.id}`}>{event.name}</Link>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div />
       </div>
     );
   }
