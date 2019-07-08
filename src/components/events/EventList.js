93fa131196d40829c3aa809ba4afc2cf94350951
helen <>
2019-07-08 14:54:42 -0700

    refactor component to receive page number from location and add links to indicate current page number in url

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -6,40 +6,88 @@ import styles from './EventList.module.css';
 
 class EventList extends React.Component {
   componentDidMount() {
-    this.props.EventListStore.getAllEvents(1);
+    const pageNumber = this.props.location.search.slice(-1);
+
+    if (pageNumber <= 3 && pageNumber > 1) {
+      this.props.EventListStore.getAllEvents(pageNumber);
+    } else {
+      this.props.EventListStore.getAllEvents(1);
+    }
   }
 
   render() {
     const { EventListStore } = this.props;
+
     return (
       <div>
         <div className="right floated content">
           <div className={styles.pagination}>
-            <span
-              onClick={() => EventListStore.getAllEvents(EventListStore.next)}
-            >
-              &laquo;
-            </span>
+            {EventListStore.currentPage - 1 > 1 ? (
+              <Link to={`/events?page=${EventListStore.currentPage - 1}`}>
+                <span
+                  onClick={() =>
+                    EventListStore.getAllEvents(EventListStore.currentPage - 1)
+                  }
+                >
+                  &laquo;
+                </span>
+              </Link>
+            ) : (
+              <Link to="/events">
+                <span onClick={() => EventListStore.getAllEvents(1)}>
+                  &laquo;
+                </span>
+              </Link>
+            )}
             {EventListStore.pageCount.map(number => {
               let classes =
                 EventListStore.currentPage === number ? styles.active : '';
-              return (
-                <span
-                  key={number}
-                  className={classes}
-                  onClick={() => EventListStore.getAllEvents(number)}
-                >
-                  {number}
-                </span>
-              );
-            })}
-            <span
-              onClick={() =>
-                EventListStore.getAllEvents(EventListStore.previous)
+              if (number <= EventListStore.pageCount.length && number > 1) {
+                return (
+                  <div key={number}>
+                    <Link to={`/events?page=${number}`}>
+                      <span
+                        className={classes}
+                        onClick={() => EventListStore.getAllEvents(number)}
+                      >
+                        {number}
+                      </span>
+                    </Link>
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={number}>
+                    <Link to="/events">
+                      <span
+                        className={classes}
+                        onClick={() => EventListStore.getAllEvents(number)}
+                      >
+                        {number}
+                      </span>
+                    </Link>
+                  </div>
+                );
               }
-            >
-              &raquo;
-            </span>
+            })}
+            {EventListStore.currentPage + 1 <=
+            EventListStore.pageCount.length ? (
+              <Link to={`/events?page=${EventListStore.currentPage + 1}`}>
+                <span
+                  onClick={() =>
+                    EventListStore.getAllEvents(EventListStore.currentPage + 1)
+                  }
+                >
+                  &raquo;
+                </span>
+              </Link>
+            ) : (
+              <Link to={`/events?page=3`}>
+                <span onClick={() => EventListStore.getAllEvents(3)}>
+                  &raquo;
+                </span>
+              </Link>
+            )}
           </div>
           <div className="ui five cards">
             {EventListStore.allEvents.map(event => {
