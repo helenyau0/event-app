1812b5078bfda9fb0256ced19202515ae39064e2
helen <>
2019-07-08 23:34:28 -0700

    add paragraphs and add header to event name

--- src/components/events/EventDetail.js
+++ src/components/events/EventDetail.js
@@ -9,25 +9,28 @@ class EventDetail extends React.Component {
 
   render() {
     const event = this.props.EventDetailStore.event;
+    const startTime = new Date(event.start_time).toLocaleDateString();
+    const endTime = new Date(event.finish_time).toLocaleDateString();
     return (
       <div>
         <img src={`${event.logo_uri}`} alt={`${event.name}`} />
-        <br />
-        {event.name}
-        <br />
-        {`Event by ${event.organizer ? event.organizer.name : 'none'}`}
-        <br />
-        {`Category: ${event.category ? event.category.name : 'none'}`}
-        <br />
-        {`Max Ticket Price: ${event.max_ticket_price} ${
-          event.ticket_price_currency
-        }`}
-        <br />
-        {`Min Ticket Price: ${event.min_ticket_price} ${
-          event.ticket_price_currency
-        }`}
-        <br />
-        <div>{event.description_plain}</div>
+        <h2>{event.name}</h2>
+        <p>
+          {startTime} — {endTime}
+        </p>
+        <p>
+          Event organized by {event.organizer ? event.organizer.name : 'none'}
+          <br />
+          Category: {event.category ? event.category.name : 'none'}
+        </p>
+        <p>
+          Max Ticket Price: {event.max_ticket_price} $
+          {event.ticket_price_currency}
+          <br />
+          Min Ticket Price: {event.min_ticket_price} $
+          {event.ticket_price_currency}
+        </p>
+        <p>{event.description_plain}</p>
         <a href={event.uri}>To Purchase click here!</a>
         <div>
           <Link to={`/events/edit/${event.id}`} className="ui button primary">
