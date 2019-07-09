05f8b3d1eaf7b375c05e58ce922ab3a004fa4aae
helen <>
2019-07-08 22:39:02 -0700

    create footer and move from top 50px

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="ui inverted vertical segment footer">
+      <p className="footer-text">© 2019 Event-App</p>
+    </div>
+  );
+};
+
+export default Footer;
